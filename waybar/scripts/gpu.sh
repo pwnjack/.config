f8ad99de090dd014848494382d174f8ee499168c
pwnjack <pwnjack@gmail.com>
2026-01-24 22:55:47 +0100

    gpu icon update color with pywall css

--- waybar/scripts/gpu.sh
+++ waybar/scripts/gpu.sh
@@ -5,4 +5,4 @@ gpu_mem_used=$(nvidia-smi --query-gpu=memory.used --format=csv,noheader,nounits 
 gpu_mem_total=$(nvidia-smi --query-gpu=memory.total --format=csv,noheader,nounits 2>/dev/null | tr -d ' ')
 gpu_name=$(nvidia-smi --query-gpu=name --format=csv,noheader 2>/dev/null)
 
-printf '{"text": "󱤓 %s%%  ", "tooltip": "GPU %s%% [%s°C]\\n\\n%s\\nVRAM: %sMB / %sMB"}' "$gpu_usage" "$gpu_usage" "$gpu_temp" "$gpu_name" "$gpu_mem_used" "$gpu_mem_total"
+printf '{"text": " %s%%  ", "tooltip": "GPU %s%% [%s°C]\\n\\n%s\\nVRAM: %sMB / %sMB"}' "$gpu_usage" "$gpu_usage" "$gpu_temp" "$gpu_name" "$gpu_mem_used" "$gpu_mem_total"
